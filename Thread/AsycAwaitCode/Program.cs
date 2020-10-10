43ffe811d49eabfa608f167d0df5210327485b15
souzaalessandro <alessand.souza@gmail.com>
2020-10-10 10:01:20 -0300

    async e await study

---
+++ Thread/AsycAwaitCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsycAwaitCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+
+        public Task SleeepAsyncA(int millisecondsTimeout)
+        {
+            return Task.Run(()=> Thread.Sleep(millisecondsTimeout));            
+        }
+
+        public Task SleepAsyncB(int millisecondsTimeout)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer( delegate { tcs.TrySetResult(true);}, null, -1,-1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(millisecondsTimeout,-1);
+            return tcs.Task;
+        }
+    }
+}
---
+++ Thread/ParalellClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParalellClass
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i =>{
+                Console.WriteLine("Parallel For");
+                Thread.Sleep(1000);
+            });
+
+            var numbers = Enumerable.Range(0,10);
+            Parallel.ForEach(numbers, i =>{
+                Console.WriteLine("Parallel ForEach");
+                Thread.Sleep(1000);
+                
+            });
+
+
+             ParallelLoopResult resultLoop = Parallel
+             .For(0,10,(int i, ParallelLoopState loppState)=>{
+
+                if (i==5)
+                {
+                    Console.WriteLine("Breaking loop");
+                    loppState.Break();
+                }
+                return;
+             });
+
+            Console.WriteLine($"Resultado {resultLoop.ToString()}");
+
+        }
+    }
+}
