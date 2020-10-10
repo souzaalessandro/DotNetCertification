41e8f8f44ab783c790df29f7a13284d96ecc4220
souzaalessandro <alessand.souza@gmail.com>
2020-10-10 10:18:15 -0300

    Modify study view

---
+++ Thread/AllClassChapter1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AllClassChapter1
+{
+    public class Class1
+    {
+    }
+}
---
+++ Thread/AllClassChapter1WebView/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AllClassChapter1WebView.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
